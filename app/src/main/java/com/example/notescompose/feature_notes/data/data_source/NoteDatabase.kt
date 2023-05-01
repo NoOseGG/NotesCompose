0213fcb3614da266509600fff1f146da139104f9
NoOseGG <hoc751@gmail.com>
2023-05-01 14:23:58 +0300

    added NoteDatabase.kt

---
+++ app/src/main/java/com/example/notescompose/feature_notes/data/data_source/NoteDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.notescompose.feature_notes.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.notescompose.feature_notes.domain.model.Note
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+}
